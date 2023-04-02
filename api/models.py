46534807ee74a0c3adb1960cc5c94bc04c4c52b5
Prateek Bhardwaj <54992024+prateekb1912@users.noreply.github.com>
2023-04-02 18:04:28 +0000

    Define hackathon model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Hackathon(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    background_image = models.ImageField(upload_to='hackathon/')
+    hackathon_image = models.ImageField(upload_to='hackathon/')
+    IMAGE = 'image'
+    FILE = 'file'
+    LINK = 'link'
+    SUBMISSION_CHOICES = [
+        (IMAGE, 'Image'),
+        (FILE, 'File'),
+        (LINK, 'Link'),
+    ]
+    submission_type = models.CharField(
+        max_length=5,
+        choices=SUBMISSION_CHOICES,
+        default=IMAGE,
+    )
+    start_datetime = models.DateTimeField()
+    end_datetime = models.DateTimeField()
+    reward_prize = models.DecimalField(max_digits=10, decimal_places=2)
